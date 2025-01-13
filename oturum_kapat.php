fee44434a5809e6ea486b59e97d35a9e3d98ce6d
HasaYasa1905Dg <156059921+HasaYasa1905Dg@users.noreply.github.com>
2025-01-14 00:32:03 +0300

    Create oturum_kapat.php

---
+++ oturum_kapat.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+<?php 
+session_destroy();
+header("Location:../index.php");
+exit();
+?>
+</body>
+</html>
